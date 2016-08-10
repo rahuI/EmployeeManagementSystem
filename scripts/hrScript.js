ad21d92185de896d57ccc38b5d0642700d0ef42a
rahul yadav <rahu5992@gmail.com>
2016-08-10 18:41:17 +0530

    app running, perhaps!

--- scripts/hrScript.js
+++ scripts/hrScript.js
@@ -1,18 +1,41 @@
 var userList=[];
 function addEmp() {
-	var email=document.getElementById("usermail").value;
-	var pass=document.getElementById("userpass").value;
+	var hemail=document.getElementById("usermail").value;
+	var hpass=document.getElementById("userpass").value;
 	var comment=document.getElementById("comment").value;
-	userList[userList.length]={em:email,pas:pass,com:comment};
-	alert(email);
-	if(userList[userList.length-1].em==email){
+	userList[userList.length]={em:hemail,pas:hpass,com:comment};
+	if(userList[userList.length-1].em==hemail){
 		document.getElementById("total").innerHTML=userList.length;
-
 		var newItem = document.createElement("li");
 		var textnode = document.createTextNode(userList[userList.length-1].em + " : " + userList[userList.length-1].com);
 		newItem.appendChild(textnode);                  
 
 		var list = document.getElementById('loginBlock');
-		list.insertBefore(newItem, list.childNodes[20]);
+		list.insertBefore(newItem, list.childNodes[22]);
 	}
+}
+function loginEmp(){
+	document.getElementById('loginBlock').style.visibility="hidden";
+	document.getElementById('loginBlock').style.display="none";
+	document.getElementById('userLogin').style.display="block";
+	document.getElementById('userLogin').style.visibility="visible";
+}
+function validateEmp(){
+	var empmail=document.getElementById("emailIn").value;
+	var emppass=document.getElementById("passIn").value;
+	for(i=0;i<userList.length;i++){
+		if(userList[i].em==empmail && userList[i].pas==emppass){
+			document.getElementById('userLogin').style.visibility="hidden";
+			document.getElementById('userLogin').style.display="none";
+
+			document.getElementById("setEmail").innerHTML=userList[i].em;
+			document.getElementById("setPass").innerHTML=userList[i].pas;
+			document.getElementById("setCom").innerHTML=userList[i].com;			
+
+			document.getElementById('userData').style.display="block";
+			document.getElementById('userData').style.visibility="visible";	
+			break;		
+		}
+	}
+	
 }
--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -1,18 +1,16 @@
 var hrEmail="hr@cronj.com";
 var hrPass="hrcronj";
 var emps={};
-var testvalue=2;
 function validateHR() {
 	var email=document.getElementById("usermail").value;
 	var pass=document.getElementById("userpass").value;
 	if(email==hrEmail && pass==hrPass) {
-			alert("Logged In!");
+			//alert("Logged In!");
 			loggedIn(email,pass);
 	}
 	else
 		alert("Wrong Email and password!");
 }
 function loggedIn(usermail,userpass){
-	testvalue=555;
 	window.location='hrHome.html';	
 }
